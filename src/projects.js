449e80a08635b9abd04e4ab9a5e3cf896e8d4e30
pj-01 <96671928+pj-pj-pj@users.noreply.github.com>
2023-10-17 15:03:04 +0800

    add projects page

--- src/projects.js
+++ src/projects.js
@@ -1,3 +1,80 @@
+import dogGif from './assets/dog.gif';
+import codePng from './assets/code.png';
+import visitPng from './assets/external-link.png';
+
 export default function projInit() {
   document.querySelector('p#active-title').textContent = 'PROJECTS.md';
+
+  const intro = document.querySelector('main #content #container');
+  intro.className = 'dynamic-page';
+
+  const dog = document.createElement('img');
+  dog.className = 'gif';
+  dog.src = dogGif;
+
+  const tagline = document.createElement('h1');
+  tagline.textContent = '<///>';
+
+  intro.append(dog, tagline);
+  intro.appendChild(document.createElement('hr'));
+
+  loadProjects();
+}
+
+async function loadProjects() {
+  try {
+    const response = await fetch('../src/assets/projects.json');
+    const data = await response.json();
+
+    // displaying projects
+    data.forEach((project) => {
+      const intro = document.querySelector('main #content #container');
+
+      const container = document.createElement('div');
+      container.className = 'project-container';
+
+      const name = document.createElement('h2');
+      name.className = 'project-title';
+      name.textContent = project.name;
+      const description = document.createElement('p');
+      description.textContent = project.description;
+
+      const detailsContainer = document.createElement('div');
+
+      const githubLinkBtn = document.createElement('div');
+      githubLinkBtn.className = 'github-btn';
+      githubLinkBtn.appendChild(document.createElement('img')).src = codePng;
+      const githubLink = document.createElement('a');
+      githubLink.textContent = 'Github Repository';
+      githubLink.href = project.githubrepo;
+      githubLink.target = '_blank';
+      githubLinkBtn.appendChild(githubLink);
+
+      const siteBtn = document.createElement('button');
+      siteBtn.className = 'site-btn';
+      siteBtn.appendChild(document.createElement('img')).src = visitPng;
+      const siteLink = document.createElement('a');
+      siteLink.textContent = 'Visit live site';
+      siteLink.href = project.siteurl;
+      siteLink.target = '_blank';
+      siteBtn.appendChild(siteLink);
+
+      const projhr = document.createElement('hr');
+      projhr.className = 'project-hr';
+      detailsContainer.append(description, githubLinkBtn);
+      container.append(detailsContainer, siteBtn);
+
+      siteBtn.addEventListener('click', () => {
+        siteLink.click();
+      });
+
+      githubLinkBtn.addEventListener('click', () => {
+        githubLink.click();
+      });
+
+      intro.append(name, container, projhr);
+    });
+  } catch (error) {
+    console.error('Error loading JSON:', error);
+  }
 }
